63b86251cdeb0be6313dee0ce921391b3c1c69bb
Ajay prakash Mishra <ajay.mishra@joshtechnology.com>
2026-08-12 11:51:04 +0530

    [AJ_A1_01]: added sidebar close feature on pressing esc key

--- src/script/index.js
+++ src/script/index.js
@@ -3,26 +3,42 @@ const nav = document.querySelector('.header__nav');
 const overlay = document.querySelector('.header__nav-overlay');
 const closeBtn = document.querySelector('.header__nav-close-btn');
 
-menu.addEventListener('click', () => {
-    if (nav.classList.contains('open')) {
-        nav.classList.remove('open');
-        overlay.style.visibility = 'hidden';
-        overlay.style.pointerEvents = 'none';
-    } else {
-        nav.classList.add('open');
-        overlay.style.visibility = 'visible';
-        overlay.style.pointerEvents = 'auto';
+menu.addEventListener('click', (e) => {
+    if (nav.classList.contains('open')) closeSidebar(e);
+    else openSidebar(e);
+});
+
+// Event listeners
+closeBtn.addEventListener('click', closeSidebar);
+overlay.addEventListener('click', closeSidebar);
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && nav.classList.contains('open')) {
+        closeSidebar(e);
     }
 });
 
-closeBtn.addEventListener('click', (e) => {
+/**
+ * Opens the navigation sidebar and enables the overlay.
+ *
+ * @param {MouseEvent} e - The click event that triggered the function.
+ * @returns {void}
+ */
+function openSidebar(e) {
     e.stopPropagation();
-    nav.classList.remove('open');
-    overlay.style.visibility = 'hidden';
-});
+    nav.classList.add('open');
+    overlay.style.visibility = 'visible';
+    overlay.style.pointerEvents = 'auto';
+}
 
-overlay.addEventListener('click', (e) => {
+/**
+ * Closes the navigation sidebar and disables the overlay.
+ *
+ * @param {MouseEvent} e - The click event that triggered the function.
+ * @returns {void}
+ */
+function closeSidebar(e) {
     e.stopPropagation();
     nav.classList.remove('open');
     overlay.style.visibility = 'hidden';
-});
+    overlay.style.pointerEvents = 'none';
+}
