fc7f083d49fe3e6940a77516ff57f34480664dac
Ajay prakash Mishra <ajay.mishra@joshtechnology.com>
2026-08-20 02:34:59 +0530

    [AJ_A1_07]: created reusable typography classes

--- src/script/index.js
+++ src/script/index.js
@@ -5,6 +5,11 @@ const nav = document.querySelector('.header__nav');
 const overlay = document.querySelector('.header__nav-overlay');
 const closeBtn = document.querySelector('.header__nav-close-btn');
 const accordianBtns = document.querySelectorAll('.accordian-btn');
+const mobileMedia = window.matchMedia('(width <= 1024px)');
+
+if (mobileMedia.matches) {
+    nav.inert = true;
+}
 
 menu.addEventListener('click', (e) => {
     if (nav.classList.contains('open')) closeSidebar(e);
@@ -14,6 +19,8 @@ menu.addEventListener('click', (e) => {
 // Event listeners
 closeBtn.addEventListener('click', closeSidebar);
 overlay.addEventListener('click', closeSidebar);
+nav.addEventListener('keydown', trapFocus);
+
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape' && nav.classList.contains('open')) {
         closeSidebar(e);
@@ -35,8 +42,14 @@ accordianBtns.forEach((btn) =>
 function openSidebar(e) {
     e.stopPropagation();
     nav.classList.add('open');
+    nav.inert = false;
+    closeBtn.focus();
     overlay.style.visibility = 'visible';
     overlay.style.pointerEvents = 'auto';
+
+    document.body.style.height = '100vh';
+
+    document.body.style.overflow = 'hidden';
 }
 
 /**
@@ -50,6 +63,12 @@ function closeSidebar(e) {
     nav.classList.remove('open');
     overlay.style.visibility = 'hidden';
     overlay.style.pointerEvents = 'none';
+
+    document.body.style.height = 'auto';
+    document.body.style.overflow = 'auto';
+
+    menu.focus();
+    nav.inert = true;
 }
 
 /**
@@ -69,6 +88,38 @@ function toggleDisclosure() {
     }
 }
 
+/**
+ * Traps the focus in navlist.
+ *
+ * @param {MouseEvent} e - The keydown event that triggered the function.
+ * @returns {void}
+ */
+function trapFocus(e) {
+    if (!mobileMedia.matches) return;
+    if (!nav.classList.contains('open')) return;
+    if (e.key !== 'Tab') return;
+
+    const focusable = nav.querySelectorAll(
+        '.header__nav-close-btn, .header__nav-link',
+    );
+
+    const first = focusable[0];
+    const last = focusable[focusable.length - 1];
+
+    if (e.shiftKey) {
+        if (document.activeElement === first) {
+            last.focus();
+            e.preventDefault();
+        }
+    } else {
+        if (document.activeElement === last) {
+            first.focus();
+            e.preventDefault();
+        }
+    }
+}
+
+// Slick setup
 $(document).ready(function () {
     $('.testimonials__slider').slick({
         slidesToShow: 1,
