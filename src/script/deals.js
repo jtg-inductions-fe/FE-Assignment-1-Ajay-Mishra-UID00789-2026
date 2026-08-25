c4e2f5ca9840a6f95f9ada3df53cc23e292a6e0a
Ajay prakash Mishra <ajay.mishra@joshtechnology.com>
2026-08-25 15:35:40 +0530

    [AJ_A1_09]: Setup wheel

--- src/script/deals.js
+++ src/script/deals.js
@@ -1,6 +1,8 @@
 const specialDealsBtn = document.querySelector('#special-deals');
 const modal = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('.modal__close');
+const wheel = document.querySelector('.wheel');
+const spinBtn = document.querySelector('.wheel__spin');
 
 specialDealsBtn.addEventListener('click', openModal);
 
@@ -13,7 +15,11 @@ modal.addEventListener('click', (e) => {
         const coupon = deal.getAttribute('data-coupon');
         copy(coupon);
     }
+    else if(e.target.closest('.wheel__spin')){
+        spinWheel();
+    }
 });
+
 
 function closeModal() {
     modal.setAttribute('aria-hidden', 'true');
@@ -35,4 +41,22 @@ async function copy(text) {
     } catch (error) {
         alert(error);
     }
+}
+
+let currentRotation = 0;
+
+const totalSections = 4;
+const sectionAngle = 360 / totalSections;
+
+function spinWheel(prizeIndex = 1) {
+    // prizeIndex: 0, 1, 2, 3
+
+    const extraSpins = 5 * 360; // 5 full rotations
+
+    // Center the chosen section under the top pointer
+    const targetAngle = 360 - (prizeIndex * sectionAngle + sectionAngle / 2);
+
+    currentRotation += extraSpins + targetAngle;
+
+    wheel.style.transform = `rotate(${currentRotation}deg)`;
 }
